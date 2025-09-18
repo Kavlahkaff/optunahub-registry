5f4f03c6cf46c6ea5bcb9b60ca35d39e28e3676d
kavlahkaff <de28oser@studserv.uni-leipzig.de>
2025-09-18 11:16:49 +0200

    mf

--- package/samplers/pfns4bo/sampler.py
+++ package/samplers/pfns4bo/sampler.py
@@ -6,8 +6,7 @@ from typing import cast
 import warnings
 
 import numpy as np
-from optuna._gp import search_space as opt_search_space
-import optuna._gp.search_space as gp_search_space
+from optuna._gp import search_space as gp_search_space
 from optuna.distributions import BaseDistribution
 from optuna.samplers import BaseSampler
 from optuna.samplers import RandomSampler
@@ -273,7 +272,7 @@ class PFNs4BOSampler(BaseSampler):
         standarized_score_vals = (score_vals - score_vals.mean()) / max(1e-10, score_vals.std())
 
         def rand_sample_func(n: int) -> torch.Tensor:
-            xs = opt_search_space.sample_normalized_params(n, internal_search_space, None)
+            xs = gp_search_space.sample_normalized_params(n, internal_search_space, None)
             ret = torch.from_numpy(xs).to(torch.float32).to(self._device)
             return ret
 
